acef934ff19df167921cadb278f39018486e5a0c
Dimitry Kudryavtsev <dkudryavstev@dataxu.com>
2012-10-31 12:00:08 -0400

    Updateding the patient workflow

--- Project1/src/com/example/project1/patient/PatientActivity.java
+++ Project1/src/com/example/project1/patient/PatientActivity.java
@@ -1,16 +1,63 @@
 package com.example.project1.patient;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.project1.R;
 
 public class PatientActivity extends Activity {
+	private final String[] items = new String[] { "Settings", "Measures",
+			"Messages" };
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_patient);
+		ListView patientListView = (ListView) findViewById(R.id.patientListView);
+		ArrayList<String> patientMenuList = new ArrayList<String>();
+		patientMenuList.addAll(Arrays.asList(items));
+		ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this,
+				R.layout.list_item, patientMenuList);
+		patientListView.setAdapter(listAdapter);
+		
+		patientListView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> adapterView, View view,
+					int position, long id) {
+				Class<?> nextActivityClass = null;
+				switch (position) {
+				case 0:
+					nextActivityClass = SettingsActivity.class;
+					break;
+				case 1:
+					nextActivityClass = MeasuresActivity.class;
+					break;
+				case 2:
+					nextActivityClass = PatientMessagesActivity.class; 
+					break;
+				default:
+					break;
+
+				}
+				if (nextActivityClass != null) {
+
+					Intent myIntent = new Intent(PatientActivity.this,
+							nextActivityClass);
+					startActivity(myIntent);
+				}
+			}
+		});
 	}
 
 	@Override
---
+++ Project1/src/com/example/project1/patient/SettingsActivity.java
@@ -0,0 +1,24 @@
+package com.example.project1.patient;
+
+import com.example.project1.R;
+import com.example.project1.R.layout;
+import com.example.project1.R.menu;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class SettingsActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_settings, menu);
+        return true;
+    }
+}
